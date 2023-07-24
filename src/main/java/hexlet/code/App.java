d587a0ba13eb063a5baedcb3b3b0d4b77bfceab8
ankoz2000 <ankoz2000@mail.ru>
2023-07-24 17:06:28 +0300

    add templates

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -1,18 +1,39 @@
 package hexlet.code;
 
 import io.javalin.Javalin;
+import io.javalin.rendering.template.JavalinThymeleaf;
+import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+import io.javalin.plugin.rendering.template.JavalinThymeleaf;
 
 public class App {
 
     public static Javalin getApp() {
         Javalin app = Javalin.create(config -> {
                     if (!isProduction()) {
-                        config.plugins.enableDevLogging();
+                        config.enableDevLogging();
                     }
+                    config.enableWebjars();
+                    JavalinThymeleaf.configure(getTemplateEngine());
                 })
                 .get("/", ctx -> ctx.result("Hello World"))
                 .start(8080);
         return app;
+    }
+
+    private static TemplateEngine getTemplateEngine() {
+        TemplateEngine templateEngine = new TemplateEngine();
+
+        ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+        templateResolver.setPrefix("/templates/");
+
+        templateEngine.addTemplateResolver(templateResolver);
+        templateEngine.addDialect(new LayoutDialect());
+        templateEngine.addDialect(new Java8TimeDialect());
+
+        return templateEngine;
     }
 
     public static void main(String[] args) {
