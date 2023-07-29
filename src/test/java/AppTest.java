cdc5c2eacd3fffaab0c75f6333c6903a057043c5
ankoz2000 <ankoz2000@mail.ru>
2023-07-29 20:09:53 +0300

    add integration tests

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,11 +1,15 @@
 import hexlet.code.App;
 import hexlet.code.model.Url;
+import hexlet.code.model.query.QUrl;
 import io.ebean.DB;
 import io.ebean.Database;
 import io.javalin.Javalin;
 import kong.unirest.HttpResponse;
 import kong.unirest.Unirest;
 import org.junit.jupiter.api.*;
+
+import java.time.Instant;
+import java.time.temporal.ChronoField;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -56,6 +60,87 @@ public class AppTest {
             HttpResponse<String> response = Unirest.get(baseUrl + "/about").asString();
             assertThat(response.getStatus()).isEqualTo(200);
             assertThat(response.getBody()).contains("Эксперименты с Javalin на Хекслете");
+        }
+    }
+
+
+    @Nested
+    class UrlTest {
+
+        @Test
+        void testIndex() {
+            HttpResponse<String> response = Unirest
+                    .get(baseUrl + "/urls")
+                    .asString();
+            String body = response.getBody();
+
+            assertThat(response.getStatus()).isEqualTo(200);
+            assertThat(body).contains("https://javalin.io");
+            assertThat(body).contains("01/01/2023 14:57");
+            assertThat(body).contains("https://commons.apache.org");
+            assertThat(body).contains("01/01/2022 13:57");
+        }
+
+        @Test
+        void testShow() {
+            HttpResponse<String> response = Unirest
+                    .get(baseUrl + "/urls/1")
+                    .asString();
+            String body = response.getBody();
+
+            assertThat(response.getStatus()).isEqualTo(200);
+            assertThat(body).contains("https://javalin.io");
+            assertThat(body).contains("01/01/2023 14:57");
+        }
+
+        @Test
+        void testNew() {
+            HttpResponse<String> response = Unirest
+                    .get(baseUrl + "/urls/new")
+                    .asString();
+            String body = response.getBody();
+
+            assertThat(response.getStatus()).isEqualTo(200);
+        }
+
+        @Test
+        void testCreate() {
+            String inputUrlName = "https://vk.com";
+            HttpResponse responsePost = Unirest
+                    .post(baseUrl + "/urls")
+                    .field("name", inputUrlName)
+                    .asEmpty();
+
+            assertThat(responsePost.getStatus()).isEqualTo(302);
+            assertThat(responsePost.getHeaders().getFirst("Location")).isEqualTo("/urls");
+
+            HttpResponse<String> response = Unirest
+                    .get(baseUrl + "/urls")
+                    .asString();
+            String body = response.getBody();
+
+            assertThat(response.getStatus()).isEqualTo(200);
+            assertThat(body).contains(inputUrlName);
+            assertThat(body).contains("Страница успешно добавлена");
+
+            Url actualUrl = new QUrl()
+                    .name.equalTo(inputUrlName)
+                    .findOne();
+
+            assertThat(actualUrl).isNotNull();
+            assertThat(actualUrl.getName()).isEqualTo(inputUrlName);
+
+            Instant today = Instant.now();
+            Instant createdAt = actualUrl.getCreatedAt();
+
+            assertThat(createdAt.get(ChronoField.YEAR))
+                    .isEqualTo(today.get(ChronoField.YEAR));
+            assertThat(createdAt.get(ChronoField.MONTH_OF_YEAR))
+                    .isEqualTo(today.get(ChronoField.MONTH_OF_YEAR));
+            assertThat(createdAt.get(ChronoField.DAY_OF_MONTH))
+                    .isEqualTo(today.get(ChronoField.DAY_OF_MONTH));
+            assertThat(createdAt.get(ChronoField.HOUR_OF_DAY))
+                    .isEqualTo(today.get(ChronoField.HOUR_OF_DAY));
         }
     }
 }
