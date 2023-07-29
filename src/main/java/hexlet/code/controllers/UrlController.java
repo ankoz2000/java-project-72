3f0da02c45b54fc706345cbffb6acf3d077f2a0a
ankoz2000 <ankoz2000@mail.ru>
2023-07-29 19:44:38 +0300

    fix add url

--- src/main/java/hexlet/code/controllers/UrlController.java
+++ src/main/java/hexlet/code/controllers/UrlController.java
@@ -36,7 +36,7 @@ public class UrlController {
             ctx.render("index.html");
             return;
         }
-        Url urlEntity = new Url(url.getProtocol() + url.getHost() + url.getPort());
+        Url urlEntity = new Url(url.getProtocol() + "://" + url.getHost() + ":" + url.getPort());
         boolean exists = new QUrl()
                 .name.equalTo(url.toString())
                 .exists();
@@ -47,7 +47,7 @@ public class UrlController {
             ctx.render("urls/index.html");
         }
 
-        log.log(System.Logger.Level.INFO, "Add url: " + url.getProtocol() + url.getHost() + url.getPort());
+        log.log(System.Logger.Level.INFO, "Add url: " + url.getProtocol() + "://" + url.getHost() + ":" + url.getPort());
 
         urlEntity.save();
 
