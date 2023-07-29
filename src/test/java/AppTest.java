d2f0faaa020974b6aea63660dec6548398743313
ankoz2000 <ankoz2000@mail.ru>
2023-07-29 19:59:38 +0300

    add appTest, change application.html and index.html

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,61 @@
+import hexlet.code.App;
+import hexlet.code.model.Url;
+import io.ebean.DB;
+import io.ebean.Database;
+import io.javalin.Javalin;
+import kong.unirest.HttpResponse;
+import kong.unirest.Unirest;
+import org.junit.jupiter.api.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AppTest {
+
+    @Test
+    void testInit() {
+        assertThat(true).isEqualTo(true);
+    }
+
+    private static Javalin app;
+    private static String baseUrl;
+    private static Url existingUrl;
+    private static Database database;
+
+    @BeforeAll
+    public static void beforeAll() {
+        app = App.getApp();
+        app.start(0);
+        int port = app.port();
+        baseUrl = "http://localhost:" + port;
+        database = DB.getDefault();
+    }
+
+    @AfterAll
+    public static void afterAll() {
+        app.stop();
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        database.script().run("/truncate.sql");
+        database.script().run("/seed-test-db.sql");
+    }
+
+    @Nested
+    class RootTest {
+
+        @Test
+        void testIndex() {
+            HttpResponse<String> response = Unirest.get(baseUrl).asString();
+            assertThat(response.getStatus()).isEqualTo(200);
+            assertThat(response.getBody()).contains("Привет от Хекслета!");
+        }
+
+        @Test
+        void testAbout() {
+            HttpResponse<String> response = Unirest.get(baseUrl + "/about").asString();
+            assertThat(response.getStatus()).isEqualTo(200);
+            assertThat(response.getBody()).contains("Эксперименты с Javalin на Хекслете");
+        }
+    }
+}
