79e5992e876d7f2b5e06a137b29a30cf6aeca89d
ankoz2000 <ankoz2000@mail.ru>
2023-07-31 22:53:12 +0300

    re-init

---
+++ app/src/main/java/hexlet/code/App.java
@@ -0,0 +1,79 @@
+package hexlet.code;
+
+import hexlet.code.controllers.MainController;
+import hexlet.code.controllers.UrlController;
+import io.javalin.Javalin;
+import io.javalin.rendering.template.JavalinThymeleaf;
+
+import nz.net.ultraq.thymeleaf.layoutdialect.LayoutDialect;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+
+import static io.javalin.apibuilder.ApiBuilder.post;
+import static io.javalin.apibuilder.ApiBuilder.get;
+import static io.javalin.apibuilder.ApiBuilder.path;
+
+public class App {
+
+    public static Javalin getApp() {
+        Javalin app = Javalin.create(config -> {
+            if (!isProduction()) {
+                config.plugins.enableDevLogging();
+            }
+
+            JavalinThymeleaf.init(getTemplateEngine());
+        });
+        addRoutes(app);
+        app.before(ctx -> {
+            ctx.attribute("ctx", ctx);
+        });
+        return app;
+    }
+
+    private static TemplateEngine getTemplateEngine() {
+        TemplateEngine templateEngine = new TemplateEngine();
+
+        ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+        templateResolver.setPrefix("/templates/");
+
+        templateEngine.addTemplateResolver(templateResolver);
+        templateEngine.addDialect(new LayoutDialect());
+        templateEngine.addDialect(new Java8TimeDialect());
+
+        return templateEngine;
+    }
+
+    public static void main(String[] args) {
+        Javalin app = getApp();
+        app.start(getPort());
+    }
+
+    private static String getMode() {
+        return System.getenv().getOrDefault("APP_ENV", "development");
+    }
+
+    private static boolean isProduction() {
+        return getMode().equals("production");
+    }
+
+    private static int getPort() {
+        String port = System.getenv().getOrDefault("PORT", "3000");
+        return Integer.valueOf(port);
+    }
+
+    private static void addRoutes(Javalin app) {
+        app.get("/", MainController.welcome);
+
+        app.routes(() -> {
+            path("urls", () -> {
+                post(UrlController.addUrl);
+                get(UrlController.listUrls);
+                get("new", UrlController.newUrl);
+                path("{id}", () -> {
+                    get(UrlController.showUrl);
+                });
+            });
+        });
+    }
+}
---
+++ app/src/test/java/AppTest.java
@@ -0,0 +1,146 @@
+import hexlet.code.App;
+import hexlet.code.model.Url;
+import hexlet.code.model.query.QUrl;
+import io.ebean.DB;
+import io.ebean.Database;
+import io.javalin.Javalin;
+import kong.unirest.HttpResponse;
+import kong.unirest.Unirest;
+import org.junit.jupiter.api.*;
+
+import java.time.Instant;
+import java.time.temporal.ChronoField;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public final class AppTest {
+
+    @Test
+    void testInit() {
+        assertThat(true).isEqualTo(true);
+    }
+
+    private static Javalin app;
+    private static String baseUrl;
+    private static Url existingUrl;
+    private static Database database;
+
+    @BeforeAll
+    public static void beforeAll() {
+        app = App.getApp();
+        app.start(0);
+        int port = app.port();
+        baseUrl = "http://localhost:" + port;
+        database = DB.getDefault();
+    }
+
+    @AfterAll
+    public static void afterAll() {
+        app.stop();
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        database.script().run("/truncate.sql");
+        database.script().run("/seed-test-db.sql");
+    }
+
+    @Nested
+    class RootTest {
+
+        @Test
+        void testIndex() {
+            HttpResponse<String> response = Unirest.get(baseUrl).asString();
+            assertThat(response.getStatus()).isEqualTo(200);
+            assertThat(response.getBody()).contains("Привет от Хекслета!");
+        }
+
+        @Test
+        void testAbout() {
+            HttpResponse<String> response = Unirest.get(baseUrl + "/about").asString();
+            assertThat(response.getStatus()).isEqualTo(200);
+            assertThat(response.getBody()).contains("Эксперименты с Javalin на Хекслете");
+        }
+    }
+
+
+    @Nested
+    class UrlTest {
+
+        @Test
+        void testIndex() {
+            HttpResponse<String> response = Unirest
+                    .get(baseUrl + "/urls")
+                    .asString();
+            String body = response.getBody();
+
+            assertThat(response.getStatus()).isEqualTo(200);
+            assertThat(body).contains("https://javalin.io");
+            assertThat(body).contains("01/01/2023 14:57");
+            assertThat(body).contains("https://commons.apache.org");
+            assertThat(body).contains("01/01/2022 13:57");
+        }
+
+        @Test
+        void testShow() {
+            HttpResponse<String> response = Unirest
+                    .get(baseUrl + "/urls/1")
+                    .asString();
+            String body = response.getBody();
+
+            assertThat(response.getStatus()).isEqualTo(200);
+            assertThat(body).contains("https://javalin.io");
+            assertThat(body).contains("01/01/2023 14:57");
+        }
+
+        @Test
+        void testNew() {
+            HttpResponse<String> response = Unirest
+                    .get(baseUrl + "/urls/new")
+                    .asString();
+            String body = response.getBody();
+
+            assertThat(response.getStatus()).isEqualTo(200);
+        }
+
+        @Test
+        void testCreate() {
+            String inputUrlName = "https://vk.com";
+            HttpResponse responsePost = Unirest
+                    .post(baseUrl + "/urls")
+                    .field("name", inputUrlName)
+                    .asEmpty();
+
+            assertThat(responsePost.getStatus()).isEqualTo(302);
+            assertThat(responsePost.getHeaders().getFirst("Location")).isEqualTo("/urls");
+
+            HttpResponse<String> response = Unirest
+                    .get(baseUrl + "/urls")
+                    .asString();
+            String body = response.getBody();
+
+            assertThat(response.getStatus()).isEqualTo(200);
+            assertThat(body).contains(inputUrlName);
+            assertThat(body).contains("Страница успешно добавлена");
+
+            Url actualUrl = new QUrl()
+                    .name.equalTo(inputUrlName)
+                    .findOne();
+
+            assertThat(actualUrl).isNotNull();
+            assertThat(actualUrl.getName()).isEqualTo(inputUrlName);
+
+            Instant today = Instant.now();
+            Instant createdAt = actualUrl.getCreatedAt();
+
+            assertThat(createdAt.get(ChronoField.YEAR))
+                    .isEqualTo(today.get(ChronoField.YEAR));
+            assertThat(createdAt.get(ChronoField.MONTH_OF_YEAR))
+                    .isEqualTo(today.get(ChronoField.MONTH_OF_YEAR));
+            assertThat(createdAt.get(ChronoField.DAY_OF_MONTH))
+                    .isEqualTo(today.get(ChronoField.DAY_OF_MONTH));
+            assertThat(createdAt.get(ChronoField.HOUR_OF_DAY))
+                    .isEqualTo(today.get(ChronoField.HOUR_OF_DAY));
+        }
+    }
+}
