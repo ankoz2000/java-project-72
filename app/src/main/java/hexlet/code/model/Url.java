a0e3b8fca9ae656fdc37a339b1e83717afbd1739
ankoz2000 <ankoz2000@mail.ru>
2023-08-10 15:21:51 +0300

    fixes

--- app/src/main/java/hexlet/code/model/Url.java
+++ app/src/main/java/hexlet/code/model/Url.java
@@ -2,11 +2,13 @@ package hexlet.code.model;
 
 import io.ebean.Model;
 import io.ebean.annotation.WhenCreated;
+import io.ebean.annotation.OneToMany;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import java.time.Instant;
+import java.util.List;
 
 @Entity
 public final class Url extends Model {
@@ -18,6 +20,9 @@ public final class Url extends Model {
 
     @WhenCreated
     private Instant createdAt;
+
+    @OneToMany(mappedBy="url")
+    private List<UrlCheck> urlCheck;
 
     public Url(String name) {
         this.name = name;
@@ -48,5 +53,13 @@ public final class Url extends Model {
 
     public void setCreatedAt(Instant createdAt) {
         this.createdAt = createdAt;
+    }
+
+    public List<UrlCheck> getUrlCheck() {
+        return urlCheck;
+    }
+
+    public void setUrlCheck(List<UrlCheck> urlCheck) {
+        this.urlCheck = urlCheck;
     }
 }
