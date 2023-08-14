dfa6ba19e7c26a92ed75631fa6e5dcc72d738e7e
ankoz2000 <ankoz2000@mail.ru>
2023-08-14 13:06:44 +0300

    fi unirest

--- src/main/java/hexlet/code/controllers/UrlController.java
+++ src/main/java/hexlet/code/controllers/UrlController.java
@@ -139,7 +139,7 @@ public class UrlController {
 
         HttpResponse responseGet = Unirest
                 .get(url.getName())
-                .asEmpty();
+                .asString();
 
         Document doc = Jsoup.parse(responseGet.getBody().toString());
 
