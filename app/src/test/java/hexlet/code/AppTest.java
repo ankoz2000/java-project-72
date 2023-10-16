ac5edeae891f954365fadef3ac6059c2e3b78745
ankoz2000 <ankoz2000@mail.ru>
2023-10-16 21:21:05 +0300

    fix sql

--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -58,7 +58,7 @@ public final class AppTest {
     @BeforeEach
     void beforeEach() throws IOException, SQLException {
         var hikariConfig = new HikariConfig();
-        hikariConfig.setJdbcUrl("jdbc:h2:mem:project;DB_CLOSE_DELAY=-1;");
+        hikariConfig.setJdbcUrl("jdbc:h2:mem:project");
         dataSource = new HikariDataSource(hikariConfig);
         var sql2 = "INSERT INTO urls (name, created_at) VALUES ('https://javalin.io', '2023-01-01 14:57')";
         try (var conn = dataSource.getConnection();
